eae000751618f1175e2bbd0c1b656e14a6d7f7fe
venkatakrishnan77088 <vkproplayer1234@gmail.com>
2022-12-26 13:10:33 +0530

    adding function types

---
+++ function/venv/py.py
@@ -0,0 +1,23 @@
+#with argument with return type
+def add(a,b):
+    return a+b
+print(add(2,5))
+
+#with argument without return type
+def add(b):
+    print(b)
+print(add(7))
+
+
+#without argument without return type
+
+def add():
+    print("7")
+    
+print(add())
+
+#without argument with return type
+
+def sum():
+    return "7"
+print(sum())
